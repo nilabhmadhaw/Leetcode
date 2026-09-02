b5fc09d6bd107f8b43d828d3ae82f147361ddb59
Nilabh Madhaw Mishra <nilabhmadhavmishra88@gmail.com>
2026-09-02 10:34:19 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,23 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-            int EvenCount = 0;
-            int OddCount = 0;
-        for(int i=0;i<nums1.length;i++){
-            if((nums1[i]&1) == 1){
-                OddCount += 1;
-            }
-            else{
-                EvenCount += 1;
-            }
-        }
-        if(EvenCount == nums1.length){
-            return true;
-        }
-        else if(OddCount == nums1.length){
-            return true;
-        }
-        else{
-                return true;
-            }
+        return true;
         }
         }
